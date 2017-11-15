f476fe1635badb27c070affb75ae7d819e0adf9d
Niels Abels <n.abels@coolblue.nl>
2017-11-15 16:09:37 +0100

    Adds example for deserializing a json string.

--- playground-serialization/Program.cs
+++ playground-serialization/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace playground_serialization
 {
@@ -6,7 +11,54 @@ namespace playground_serialization
     {
         static void Main(string[] args)
         {
-            
+            var jsonContents = File.ReadAllText("todeserialize.json");
+
+            var productSalesDto = JsonConvert.DeserializeObject<ProductSalesDto>(jsonContents);
+
+            var dailySupplierSales = productSalesDto.SupplierSales.SelectMany(ss => ss.DailySales).GroupBy(ds => ds.Day)
+                .Select(grouping =>
+                    new SalesPerDay {Day = grouping.Key, Sales = grouping.Sum(group => group.Quantity)}).OrderByDescending(salesPerDay => salesPerDay.Day).ToList();
+
+            foreach (var dailySupplierSale in dailySupplierSales)
+            {
+                Console.WriteLine($"{nameof(SalesPerDay)}: {dailySupplierSale.Day}: {dailySupplierSale.Sales}");
+            }
         }
+    }
+
+    public class SalesPerDay
+    {
+        public DateTime Day { get; set; }
+        public int Sales { get; set; }
+    }
+
+    public class ProductSalesDto
+    {
+        public ProductDto Product { get; set; }
+        public IEnumerable<SupplierSalesDto> SupplierSales { get; set; }
+    }
+
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class SupplierSalesDto
+    {
+        public SupplierDto Supplier { get; set; }
+        public IEnumerable<DailySupplierSalesDto> DailySales { get; set; }
+    }
+    
+    public class SupplierDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+    public class DailySupplierSalesDto
+    {
+        public DateTime Day { get; set; }
+        public int Quantity { get; set; }
     }
 }
